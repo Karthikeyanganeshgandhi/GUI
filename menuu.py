868152c2950a381c82db81d230c7c06e1228e355
Karthikeyan G <91650724+Karthikeyanganeshgandhi@users.noreply.github.com>
2024-12-05 16:53:05 +0530

    Add files via upload

---
+++ menuu.py
@@ -0,0 +1,67 @@
+
+
+
+
+# from tkinter import *
+  
+# root = Tk() 
+# root.geometry("300x200") 
+  
+
+  
+# m1 = Menubutton(root, text = "About")    
+    
+# m1.menu = Menu(m1)   
+# m1["menu"]= m1.menu 
+  
+# var1 = IntVar() 
+# var2 = IntVar() 
+# var3 = IntVar() 
+  
+# m1.menu.add_checkbutton(label= "Courses", 
+#                                 variable = var1)   
+# m1.menu.add_checkbutton(label = "Students", 
+#                                 variable = var2) 
+# m1.menu.add_checkbutton(label = "Careers", 
+#                                 variable = var3) 
+    
+# m1.pack()   
+# root.mainloop() 
+
+
+
+# from tkinter import *
+# from tkinter import ttk
+
+# root = Tk()
+#Paned Window
+# pw = ttk.PanedWindow(root, orient=HORIZONTAL)
+# pw.pack(fill=BOTH, expand=True)
+# frame1 = ttk.Frame(pw, relief=SUNKEN)
+# frame2 = ttk.Frame(pw, relief=SUNKEN)
+# pw.add(frame1, weight=1)
+# pw.add(frame2,weight=3)
+
+# root.geometry("400x240")
+# root.title("PythonLobby.com")
+# root.mainloop()
+
+
+# from tkinter import *
+# from tkinter import ttk
+
+# root = Tk()
+
+
+# listbox = Listbox(root, width=45, height=15,selectmode=MULTIPLE)
+# listbox.pack(pady=20)
+
+
+# listbox.insert(0, "C")
+# listbox.insert(1, "C++")
+# listbox.insert(2, "Java")
+# listbox.insert(3, "Python")
+
+# root.geometry("400x240")
+# root.title("PythonLobby.com")
+# root.mainloop()
